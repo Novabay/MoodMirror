341e5e0d674df8ad514e4ca995ce40531ca32495
neumuelleran87784 <neumuelleran87784@th-nuernberg.de>
2023-06-15 12:04:07 +0200

    update repo on own acount

--- mm.py
+++ mm.py
@@ -7,12 +7,17 @@ picam = Picamera2()
 config = picam.create_preview_configuration()
 picam.configure(config)
 
-#cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0) normaly used dosent work 
 
 picam.start()
 while True:
     frame=picam.capture_array()
     rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    try:
+        prediction = DeepFace.analyze(rgb,actions='emotion',enforce_detection=False)
+        print(prediction[0]['dominant_emotion'])
+    except ValueError:
+        print('Face could not be detected.')
     cv2.imshow('Cam0',rgb)
     if (cv2.waitKey(1) == ord('q')):
         break
